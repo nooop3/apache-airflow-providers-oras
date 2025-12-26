01f958990c96e850dfea2ffa5769892d31a8670d
Edward Guan <nooop3.lau@gmail.com>
2025-12-26 16:04:48 +0800

    refactor: Add Airflow version check to __init__.py

--- src/airflow/providers/oras/__init__.py
+++ src/airflow/providers/oras/__init__.py
@@ -1,5 +1,17 @@
 """Apache Airflow provider for ORAS."""
 
+from __future__ import annotations
+
+import packaging.version
+
+from airflow import __version__ as airflow_version
 from airflow.providers.oras.__version__ import __version__
 
 __all__ = ["__version__"]
+
+if packaging.version.parse(packaging.version.parse(airflow_version).base_version) < packaging.version.parse(
+    "3.0.0"
+):
+    raise RuntimeError(
+        f"The package `apache-airflow-providers-oras:{__version__}` needs Apache Airflow 3.0.0+"
+    )
